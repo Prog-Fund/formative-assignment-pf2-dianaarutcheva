89fd2c1b7dbbd0612fbdf8d9ab4b99c9a63c7fa9
Diana Arutcheva <20106796@mail.wit.ie>
2024-02-08 16:53:52 +0000

    productCode between 1000 and 5000 inclusive, default 5000

--- src/Product.java
+++ src/Product.java
@@ -5,13 +5,13 @@
  */
 public class Product {
 
-    private String productName ;    // max length = 20 characters, default value is "".
+    private String productName = "";    // max length = 20 characters, default value is "".
                                     // When constructor is called, if the name is >20 chars, you should
                                     // only store the first 20 characters (Hint: use substr())
 
-    private int productCode ;  // valid values 1000 - 5000 inclusive - default value is 5000
+    private int productCode = 5000;  // valid values 1000 - 5000 inclusive - default value is 5000
 
-    private double unitCost;  //valid values are any positive number - default to 1
+    private double unitCost = 1;  //valid values are any positive number - default to 1
 
     private boolean inCurrentProductLine;   // no validation required. Default
     /**
